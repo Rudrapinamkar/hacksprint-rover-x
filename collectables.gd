e8555f6407ffa0eb03e0c88fec41b6743934c275
Rudrapinamkar <rudrapinamkar88@gmail.com>
2026-04-09 23:10:10 +0530

    init

--- collectables.gd
+++ collectables.gd
@@ -1,13 +1,10 @@
 extends Node
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
+func _on_area_entered(area: Area2D) -> void:
+	print("hi")
+	Global.Data+=1
+	queue_free()
--- node_2d.gd
+++ node_2d.gd
@@ -6,4 +6,5 @@ func _process(delta: float) -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("data"):
 		Global.Data+=1
+		print("hhi")
 		
